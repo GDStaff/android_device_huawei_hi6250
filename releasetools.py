afce94675fdacc92fed5b44953ead158b98cac23
Meticulus <theonejohnnyd@gmail.com>
2018-02-20 11:08:46 -0600

    releasetools: Touchups and branding..

--- releasetools.py
+++ releasetools.py
@@ -1,7 +1,6 @@
 # Copyright (C) 2012 The Android Open Source Project
 # Copyright (C) 2012 The CyanogenMod Project
 # Copyright (C) 2014 Jonathan Jason Dennis [Meticulus] (theonejohnnyd@gmail.com)
-# Copyright (C) 2018 Béleczki Péter [petya230] (trolololo@gmail.com)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -68,24 +67,32 @@ def FullOTA_Assertions(info):
 
 
 def FullOTA_InstallBegin(info):
-  info.script.AppendExtra('ui_print("                 Join our community at:             ");');
-  info.script.AppendExtra('ui_print("                https://codeofhonor.tech            ");');
-  info.script.AppendExtra('ui_print("                                                    ");');
-  info.script.AppendExtra('ui_print("                      THE TEAM:                     ");');
-  info.script.AppendExtra('ui_print("         felix-development: [Honor 6X Maintainer]   ");');
-  info.script.AppendExtra('ui_print("         Haky 86          : [Tester]                ");');
-  info.script.AppendExtra('ui_print("         HassanMirza01    : [All Rounder (Pakistan)]");');
-  info.script.AppendExtra('ui_print("         LRX22G           : [Graphics Master]       ");');
-  info.script.AppendExtra('ui_print("         Meticulus        : [IMPEACH TRUMP!]        ");');
-  info.script.AppendExtra('ui_print("         Petya230         : [Meeh!]                 ");');
-  info.script.AppendExtra('ui_print("         r33int (Enzo)    : [Tester, Sysadmin]      ");');
-  info.script.AppendExtra('ui_print("         xD3SH            : [Tester]                ");');
-  info.script.AppendExtra('ui_print("         ToeiRei          : [AI Nerd Girl]          ");');
-  info.script.AppendExtra('ui_print("         Tw1st3d83        : [Tester]                ");');
   info.script.AppendExtra('ui_print("                                                    ");');
   info.script.AppendExtra('ui_print("                                                    ");');
-  info.script.AppendExtra('ui_print("                  Additional Credits:               ");');
-  info.script.AppendExtra('ui_print("                  XePeleato [OpenKirin]             ");');
+  info.script.AppendExtra('ui_print("               Join our community at:               ");');
+  info.script.AppendExtra('ui_print("              https://codeofhonor.tech              ");');
+  info.script.AppendExtra('ui_print("                                                    ");');
+  info.script.AppendExtra('ui_print("   THE TEAM:                                        ");');
+  info.script.AppendExtra('ui_print("                                                    ");');
+  info.script.AppendExtra('ui_print("   felix-development : [Honor 6X Maintainer]        ");');
+  info.script.AppendExtra('ui_print("   Haky 86           : [Tester]                     ");');
+  info.script.AppendExtra('ui_print("   HassanMirza01     : [All Rounder (Pakistan)]     ");');
+  info.script.AppendExtra('ui_print("   LRX22G            : [Graphics Master]            ");');
+  info.script.AppendExtra('ui_print("   Meticulus         : [IMPEACH TRUMP!]             ");');
+  info.script.AppendExtra('ui_print("   Petya230          : [Hate Hassan! <3]            ");');
+  info.script.AppendExtra('ui_print("   r33int (Enzo)     : [Tester, Sysadmin]           ");');
+  info.script.AppendExtra('ui_print("   xD3SH             : [Tester]                     ");');
+  info.script.AppendExtra('ui_print("   ToeiRei           : [AI Nerd Girl]               ");');
+  info.script.AppendExtra('ui_print("   Tw1st3d83         : [Tester]                     ");');
+  info.script.AppendExtra('ui_print("                                                    ");');
+  info.script.AppendExtra('ui_print("                                                    ");');
+  info.script.AppendExtra('ui_print("   ADDITIONAL CREDITS:                              ");');
+  info.script.AppendExtra('ui_print("                                                    ");');
+  info.script.AppendExtra('ui_print("   XePeleato         : [OpenKirin]                  ");');
+  info.script.AppendExtra('ui_print("                                                    ");');
+  info.script.AppendExtra('ui_print("                                                    ");');
+# Sleep 5 seconds to make sure it is seen
+  info.script.AppendExtra('run_program("/sbin/sleep", "5");')
                                                              
 # CoH Team Members: Please insert yours and any other credits here! Alphabetical order pls ;)
 
