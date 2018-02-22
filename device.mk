19ae81c672e54ded11d6dda0b7906c3dc383cc78
Meticulus <theonejohnnyd@gmail.com>
2018-02-22 05:35:06 -0600

    device: remove sensorlib
    -in favor of sensorlib from oreo

--- device.mk
+++ device.mk
@@ -75,9 +75,6 @@ PRODUCT_PACKAGES += \
     hwcomposer.hi6250 \
     libcopybit_wrapper \
     lights.hi6250 \
-    android.hardware.sensors@1.0-service \
-    android.hardware.sensors@1.0-impl \
-    sensors.hi6250 \
     metiotgd \
     emui5_vendor_symlinks \
     android.hardware.graphics.allocator@2.0-impl \
