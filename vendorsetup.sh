a03cd090fc24c2140353bad9d792b576912e3f51
Dil3mm4 <dil3mm4.dev@gmail.com>
2018-03-20 23:06:23 +0100

    Viper bringup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,6 +6,8 @@ elif [[ -e vendor/cm ]]; then
     add_lunch_combo lineage_hi6250-userdebug
 elif [[ -e vendor/validus ]]; then
     add_lunch_combo validus_hi6250-userdebug
+elif [[ -e vendor/viper ]]; then 
+    add_lunch_combo viper_hi6250-userdebug
 else
     add_lunch_combo aosp_hi6250-userdebug
 fi
