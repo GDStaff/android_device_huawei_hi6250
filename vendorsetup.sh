53ef71503b16f1be15a071978074d1f913b87e02
Dil3mm4 <dil3mm4.dev@gmail.com>
2018-03-31 17:49:44 +0200

    AIM breaking things (6/6)

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,7 +3,7 @@ if [[ -e vendor/slim ]]; then
 elif [[ -e vendor/omni ]]; then
     add_lunch_combo omni_hi6250-userdebug
 elif [[ -e vendor/cm ]]; then
-    add_lunch_combo aim_hi6250-userdebug
+    add_lunch_combo lineage_hi6250-userdebug
 elif [[ -e vendor/validus ]]; then
     add_lunch_combo validus_hi6250-userdebug
 elif [[ -e vendor/viper ]]; then 
