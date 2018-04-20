ce28ea8a40ed27fe54c931f02f05b0e029b561e4
felix-development <felixprivateskonto@gmail.com>
2018-04-20 14:24:39 +0200

    cleanup: removing a big dilemma

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,8 +6,6 @@ elif [[ -e vendor/cm ]]; then
     add_lunch_combo lineage_hi6250-userdebug
 elif [[ -e vendor/validus ]]; then
     add_lunch_combo validus_hi6250-userdebug
-elif [[ -e vendor/aim ]]; then 
-    add_lunch_combo aim_hi6250-userdebug
 else
     add_lunch_combo aosp_hi6250-userdebug
 fi
